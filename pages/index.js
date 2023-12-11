684f331eae156c4649ed1dc086a06ff55e0f939f
NewBean0312 <newbean0312@gmail.com>
2023-12-11 17:24:28 +0900

    api 적용 및 상세페이지 작업

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,43 @@
+import Axios from "axios";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import ItemList from "@/src/component/ItemList";
 import styles from "@/styles/Home.module.css";
+import { Divider, Header } from "semantic-ui-react";
 
 export default function Home() {
+  const [list, setList] = useState([]);
+
+  const API_URL =
+    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+
+  function getData() {
+    // 상품을 받아옴
+    Axios.get(API_URL).then((res) => {
+      console.log(res.data);
+      setList(res.data);
+    });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <Head>
         <title>NewBean</title>
       </Head>
-      안녕하세요! NewBean의 Next.js 공부입니다!
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        베스트 상품
+      </Header>
+      <Divider />
+      <ItemList list={list.slice(0, 9)} />
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        신상품
+      </Header>
+      <Divider />
+      <ItemList list={list.slice(9)} />
     </div>
   );
 }
