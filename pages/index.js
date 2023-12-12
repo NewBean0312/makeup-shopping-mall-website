ae16b8603b6af9756c6d408fff18c9b917c181cf
NewBean0312 <newbean0312@gmail.com>
2023-12-12 20:16:44 +0900

    로딩 페이지 및 About 페이지 작업

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import ItemList from "@/src/component/ItemList";
 import styles from "@/styles/Home.module.css";
-import { Divider, Header } from "semantic-ui-react";
+import { Divider, Header, Loader } from "semantic-ui-react";
 
 export default function Home() {
   const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const API_URL =
     "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
@@ -16,6 +17,7 @@ export default function Home() {
     Axios.get(API_URL).then((res) => {
       console.log(res.data);
       setList(res.data);
+      setIsLoading(false);
     });
   }
 
@@ -27,17 +29,29 @@ export default function Home() {
     <div>
       <Head>
         <title>NewBean</title>
+        <meta name="description" content="NewBean 홈입니다."></meta>
       </Head>
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        베스트 상품
-      </Header>
-      <Divider />
-      <ItemList list={list.slice(0, 9)} />
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        신상품
-      </Header>
-      <Divider />
-      <ItemList list={list.slice(9)} />
+      {isLoading && (
+        <div style={{ padding: "300px 0" }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      )}
+      {!isLoading && (
+        <>
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            베스트 상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(0, 9)} />
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            신상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(9)} />
+        </>
+      )}
     </div>
   );
 }
--- src/component/Gnb.js
+++ src/component/Gnb.js
@@ -1,23 +1,36 @@
+import { useRouter } from "next/router";
 import { Menu } from "semantic-ui-react";
 
 export default function Gnb() {
-  const activeItem = "home";
+  const router = useRouter();
+  let activeItem;
+
+  if (router.pathname === "/") {
+    activeItem = "home";
+  } else if (router.pathname === "/about") {
+    activeItem = "about";
+  }
+
+  // 버튼 클릭 시, 원하는 링크로 이동하기
+  function goLink(e, data) {
+    if(data.name === 'home') {
+      router.push("/");
+    } else if (data.name === 'about') {
+      router.push("/about");
+    }
+  }
+
   return (
     <Menu inverted>
       <Menu.Item
         name="home"
         active={activeItem === "home"}
-        // onClick={this.handleItemClick}
+        onClick={goLink}
       />
       <Menu.Item
-        name="messages"
-        active={activeItem === "messages"}
-        // onClick={this.handleItemClick}
-      />
-      <Menu.Item
-        name="friends"
-        active={activeItem === "friends"}
-        // onClick={this.handleItemClick}
+        name="about"
+        active={activeItem === "about"}
+        onClick={goLink}
       />
     </Menu>
   );
