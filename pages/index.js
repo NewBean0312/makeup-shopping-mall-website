c3a413d4e9ce0b2925a3e28251744795ecec4a9b
NewBean0312 <newbean0312@gmail.com>
2023-12-13 16:30:21 +0900

    에러 페이지, 환경 변수 작업

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,7 @@ export default function Home() {
   const [list, setList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const API_URL =
-    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
   function getData() {
     // 상품을 받아옴
@@ -33,6 +32,7 @@ export default function Home() {
       </Head>
       {isLoading && (
         <div style={{ padding: "300px 0" }}>
+          {/* 로딩화면 추가 */}
           <Loader inline="centered" active>
             Loading
           </Loader>
--- src/component/Gnb.js
+++ src/component/Gnb.js
@@ -13,24 +13,27 @@ export default function Gnb() {
 
   // 버튼 클릭 시, 원하는 링크로 이동하기
   function goLink(e, data) {
-    if(data.name === 'home') {
+    if (data.name === "home") {
       router.push("/");
-    } else if (data.name === 'about') {
+    } else if (data.name === "about") {
       router.push("/about");
     }
   }
 
   return (
     <Menu inverted>
-      <Menu.Item
-        name="home"
-        active={activeItem === "home"}
-        onClick={goLink}
-      />
+      <Menu.Item name="home" active={activeItem === "home"} onClick={goLink} />
       <Menu.Item
         name="about"
         active={activeItem === "about"}
         onClick={goLink}
+      />
+      <Menu.Item
+        name="Contact us"
+        active={activeItem === "contact"}
+        onClick={() => {
+          router.push("/contact");
+        }}
       />
     </Menu>
   );
